b6d79ee9c86bfd54619b96aaad9721ccb99d7b69
Arinze <arinzedile@yahoo.com>
2017-12-28 04:04:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children
+    for 
   }
 }
